79dd0aa2e48521f3d3e3d2bbd5dce3408c42a869
KaioNog <kanog.404@gmail.com>
2023-04-18 06:12:04 -0300

    upd project 17-04-23
    Player Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,20 @@ public class PlayerController : MonoBehaviour
 {
 
     public float runSpeed;
+    public float walkSpeed;
 
     Rigidbody myRb;
     Animator myAnim;
 
     bool facingRight;
+
+    //for jumping
+    bool grounded = false;
+    Collider[] groundCollisions;
+    float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpHeight;
 
     void Start()
     {
@@ -24,12 +33,42 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    //o Update é chamado a cada frame, o fixed update é chamado toda vez que um mecanismo físico for executado (run) em um tempo fixo e constante (se atualmente está pressionando o botão)
     void FixedUpdate()
     {
+        //OverlapSphere desenha uma pequena esfere e retorna a colisão
+        groundCollisions = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer);
+        if(groundCollisions.Length>0) 
+            grounded = true;
+        else grounded = false;
+        myAnim.SetBool("grounded", grounded);
+
+        //jump
+        if(grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            myAnim.SetBool("grounded", grounded);
+            //myAnim.SetFloat("verticalSpeed", myRB.velocity.y);
+            myRb.AddForce(new Vector3(0, jumpHeight, 0));
+        }
+
+        //run and walk
         float move = Input.GetAxis("Horizontal");
         myAnim.SetFloat("speed",Mathf.Abs (move));
 
         myRb.velocity = new Vector3(move * runSpeed, myRb.velocity.y, 0);
+
+        float sneaking = Input.GetAxisRaw("Fire3");
+        myAnim.SetFloat("sneaking",sneaking);
+
+        if(sneaking > 0 && grounded)
+        {
+            myRb.velocity = new Vector3 (move * walkSpeed, myRb.velocity.y, 0);
+        }
+        else
+        {
+            myRb.velocity = new Vector3 (move * runSpeed, myRb.velocity.y, 0);
+        }
 
         if(move > 0 && !facingRight) 
             Flip();
@@ -39,6 +78,7 @@ public class PlayerController : MonoBehaviour
 
     void Flip()
     {
+        //transforma a escala z - -1 = rotacionar em 180 graus
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
         theScale.z *= -1;
